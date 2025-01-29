42b83308a6e5485507c989de9428c3db1cceb5b0
Aashutosh soni <ashutoshsoni130@gmail.com>
2025-01-29 11:07:57 +0530

    update: live sequence changed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const fetchLiveUrls = require('./api/fetchLiveUrls');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9826;
 
 app.use(cors());
 
